1cf5197b4a06afb65930c39201de736ef1695cd3
tobias-dv-lnu <tobias.ohlsson@lnu.se>
2014-05-16 14:28:30 +0200

    Test

--- src/_classDiagram.php
+++ src/_classDiagram.php
@@ -3,5 +3,8 @@
 require_once("model/Folder.php");
 require_once("view/ClassDiagram.php");
 
+// Dis is a test comment
+
+
 new \view\ClassDiagram(new \model\Folder($_GET["basepath"]), 
 					   $_GET["selected"]);
